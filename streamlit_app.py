479670f84339f1d173ecb5a57f91ef9a6afe7062
AndyCognitivo <anderson.almeida@cognitivo.ai>
2024-09-25 10:40:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ st.markdown(
 st.title('Smart Mix')
 st.text('Gerando a melhor sugestão de estoque para o seu negócio farmaceutico.')
 
-st.multiselect('Selecione o brick:', [1,2,3,4,5])
+brick = st.multiselect('Selecione o brick:', [1,2,3,4,5])
+utc = st.multiselect('Selecione o UTC:', [6,7,8,9,10])
+perfil_loja = st.multiselect('Selecione o perfil de loja:', ['A','B','C','D','E'])
 
 st.sidebar.title('Settings')
 length = st.sidebar.slider('Length', 4, 64, 8, 1)
