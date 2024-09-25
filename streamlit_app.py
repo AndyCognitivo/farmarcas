65b3de4db3f5205eedd19d855ecd31cc7f537792
AndyCognitivo <anderson.almeida@cognitivo.ai>
2024-09-25 11:38:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,6 +85,6 @@ df = df_pareto_func(merge_fato_cadastro, 'brick', 'categoria_ajustada', 'sum_uni
 
 
 #df_example = pd.DataFrame(np.array( [[1, 2, 3, 45], [4, 5, 6, 43], [3, 6, 2, 12]] ),
-                   columns=[ 'brick', 'utc', 'perfil loja', 'quantidade'])
+#                   columns=[ 'brick', 'utc', 'perfil loja', 'quantidade'])
 
 st.dataframe(df)
